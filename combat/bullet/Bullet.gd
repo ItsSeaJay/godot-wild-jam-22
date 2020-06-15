e2a0625ba9e9d19a3bccb31b1f5116374257fc34
ItsSeaJay <callum@sea-jay.net>
2020-06-15 10:01:22 +0100

    Add team switching

--- combat/bullet/Bullet.gd
+++ combat/bullet/Bullet.gd
@@ -7,6 +7,9 @@ var velocity = Vector2.ZERO
 func _ready():
 	# Allow the target group to be edited by nodes higher up the tree
 	$Hitbox.target_group = target_group
+	
+	yield(get_tree().create_timer(60.0), "timeout")
+	queue_free()
 
 func _process(delta):
 	look_at(global_position + velocity.normalized())
--- player/Player.gd
+++ player/Player.gd
@@ -2,19 +2,31 @@ class_name Player
 extends KinematicBody2D
 
 const Bullet = preload("res://combat/bullet/Bullet.tscn")
+const SPRITES = {
+	"team_first": preload("res://player/paddleRed.png"),
+	"team_second": preload("res://player/paddleBlu.png")
+}
 
 export var torque = 1 # How many times each second the player should spin
-export var movement_speed = 32.0
-export var cooldown_time = 0.1
-export var shot_speed = 512.0
+export var movement_speed = 512.0
+export var movement_acceleration = 128.0
+export var movement_friction = 128.0
+export var cooldown_time = 0.01
+export var shot_speed = 1024.0
 
 var spin_direction = 1 # Determines which way the player should spin where '1' is clockwise
 var heat = 0
+var movement_velocity : Vector2 = Vector2.ZERO
+var team = "team_first"
 
-func _process(delta):
+func _physics_process(delta):
 	process_firing(delta)
 	process_movement(delta)
 	process_spinning(delta)
+	process_groups(delta)
+	
+	if Input.is_action_just_pressed("player_reverse_direction"):
+		flip()
 
 func process_firing(delta):
 	if heat == 0:
@@ -23,8 +35,26 @@ func process_firing(delta):
 		# Cool down the gun by delta
 		heat = max(0, heat - delta)
 
+func process_movement(delta):
+	var input_direction = get_input_direction()
+	var linear_velocity = movement_velocity
+	
+	# Normalize the input direction
+	input_direction = input_direction.normalized()
+	
+	if input_direction != Vector2.ZERO:
+		linear_velocity = linear_velocity.move_toward(input_direction * movement_speed, movement_acceleration)
+	else:
+		linear_velocity = linear_velocity.move_toward(Vector2.ZERO, movement_friction)
+	
+	movement_velocity = move_and_slide(linear_velocity)
+
 func process_spinning(delta):
-	self.rotate(TAU * torque * delta)
+	self.rotate(TAU * torque * delta * spin_direction)
+
+func process_groups(delta):
+	if SPRITES.has(team):
+		$Sprite.texture = SPRITES[team]
 
 func fire():
 	var instance = Bullet.instance()
@@ -33,5 +63,25 @@ func fire():
 	
 	get_tree().root.add_child(instance)
 	
-	# Heat up the guns
+	# Prevent the player from firing until the heat has surpised the cooldown time
 	heat = cooldown_time
+
+func flip():
+	spin_direction *= -1
+	
+	# Change to the opposite team the player is currently in
+	# Change to the opposite team the player is currently in
+	if team == "team_first":
+		team = "team_second"
+	elif team == "team_second":
+		team = "team_first"
+
+func get_input_direction():
+	var input_direction = Vector2.ZERO
+	
+	# Get the input direction based on the strength of the axis
+	# Thanks, HeartBeast! (https://www.youtube.com/watch?v=mAbG8Oi-SvQ)
+	input_direction.x = Input.get_action_strength("player_move_right") - Input.get_action_strength("player_move_left")
+	input_direction.y = Input.get_action_strength("player_move_down") - Input.get_action_strength("player_move_up")
+	
+	return input_direction
